81ad139ec08ebdd142182d5c4840a05b278ccb8b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-08-23 09:24:54 +0900

    adjust login form style

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Header, Button, Form } from 'semantic-ui-react'
+import "./Login.css";
 
 class Login extends Component {
 
@@ -9,10 +10,10 @@ class Login extends Component {
 
   render () {
     return (
-      <Container text>
+      <Container text className="Login-container">
         <Header as='h2' icon textAlign='center' color='teal'>
           <Header.Content>Okini-arts</Header.Content>
-          <Form>
+          <Form className="Login-form">
             <Button id="qsLoginBtn" className="btn-margin" onClick={this.login.bind(this)}>
               Log In
             </Button>

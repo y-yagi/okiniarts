208ad5dd9fd02c72c7fb87f926375e6230e8e8ce
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-05 10:52:05 +0900

    fix callback URL in production

--- client/src/auth/auth0-variables.js
+++ client/src/auth/auth0-variables.js
@@ -1,5 +1,6 @@
+const callbackUrl = process.env.NODE_ENV === 'production' ? 'https://okini-arts.herokuapp.com/callback' : 'http://localhost:3000/callback'
 export const AUTH_CONFIG = {
   domain: 'okiniarts.auth0.com',
   clientId: 'JBfFodKF3HidREciZunc_0u1OX8PaKhO',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: callbackUrl
 }

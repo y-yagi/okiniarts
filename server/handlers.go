cfa54e2f4f73a67920c2133113a53a181bca0f23
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-05 10:06:15 +0900

    add CRUD for arts

--- server/handlers.go
+++ server/handlers.go
@@ -17,16 +17,20 @@ func addRoutes(e *echo.Echo) {
 		SigningKey:    auth0Key(),
 	}
 	r.Use(middleware.JWTWithConfig(config))
-	r.GET("/arts", arts)
-	r.GET("/arts/:id", art)
+	r.GET("/arts", getArts)
+	r.GET("/arts/:id", getArt)
+	r.POST("/arts", createArt)
+	r.PUT("/arts/:id", updateArt)
+	r.DELETE("/arts/:id", deleteArt)
+
 	r.GET("/artists", getArtists)
-	r.POST("/artists", createArtist)
 	r.GET("/artists/:id", getArtist)
+	r.POST("/artists", createArtist)
 	r.PUT("/artists/:id", updateArtist)
 	r.DELETE("/artists/:id", deleteArtist)
 }
 
-func arts(c echo.Context) error {
+func getArts(c echo.Context) error {
 	var arts []Art
 	if err := db.Model(&arts).Relation("Artist").Where("art.user_identifier = ?", extractUserID(c)).Select(); err != nil {
 		return err
@@ -35,7 +39,7 @@ func arts(c echo.Context) error {
 	return c.JSON(http.StatusOK, arts)
 }
 
-func art(c echo.Context) error {
+func getArt(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 	art := &Art{Id: id}
 	if err := db.Model(art).Relation("Artist").Where("art.user_identifier = ? AND art.id = ?", extractUserID(c), id).Select(); err != nil {
@@ -43,6 +47,52 @@ func art(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, art)
+}
+
+func createArt(c echo.Context) error {
+	a := &Art{
+		UserIdentifier: extractUserID(c),
+	}
+	if err := c.Bind(a); err != nil {
+		return err
+	}
+
+	err := db.Insert(a)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, a)
+}
+
+func updateArt(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	a := new(Art)
+	if err := db.Model(a).Where("user_identifier = ? AND id = ?", extractUserID(c), id).Select(); err != nil {
+		return err
+	}
+
+	if err := c.Bind(a); err != nil {
+		return err
+	}
+
+	err := db.Update(a)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, a)
+}
+
+func deleteArt(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	a := new(Art)
+	if err := db.Model(a).Where("user_identifier = ? AND id = ?", extractUserID(c), id).Select(); err != nil {
+		return err
+	}
+
+	db.Delete(a)
+	return c.NoContent(http.StatusNoContent)
 }
 
 func getArtists(c echo.Context) error {

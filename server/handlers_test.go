892622887e8c45e6ecd3bcdb2568388c938e708d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-14 15:58:23 +0900

    add test for arts API

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
+	"os"
 	"testing"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
-type auth struct {
-	audience     string
-	grantType    string
-	clientID     string
-	clientSecret string
+func generateTestData() {
+	db.ExecOne("TRUNCATE TABLE artists")
+	db.ExecOne("TRUNCATE TABLE arts")
+
+	a := &Artist{
+		UserIdentifier: "1",
+		Name:           "クロード・モネ",
+		Detail:         "印象派を代表するフランスの画家",
+	}
+	if err := db.Insert(a); err != nil {
+		panic(err)
+	}
+
+	art := &Art{
+		UserIdentifier: "1",
+		Name:           "積みわら",
+		Detail:         "収穫後の畑に積まれた干し草の山を描いた一連の絵画の総称",
+		ArtistID:       a.ID,
+	}
+	if err := db.Insert(art); err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	initDB()
+	defer db.Close()
+	generateTestData()
+
+	code := m.Run()
+
+	defer os.Exit(code)
 }
 
 func TestGetArts(t *testing.T) {
@@ -23,7 +55,9 @@ func TestGetArts(t *testing.T) {
 		Host:   addr.String(),
 		Path:   "/api/arts",
 	}
+
 	req, _ := http.NewRequest("GET", reqURL.String(), nil)
+	req.Header.Add("Authorization", "Bearer "+generateToken(t))
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -35,8 +69,17 @@ func TestGetArts(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	actual := string(body)
-	expected := `{"message":"missing or malformed jwt"}`
+	var arts []Art
+	json.Unmarshal([]byte(string(body)), &arts)
+
+	expected := "積みわら"
+	actual := arts[0].Name
+	if actual != expected {
+		t.Errorf("got: %s\nwont: %s", actual, expected)
+	}
+
+	expected = "収穫後の畑に積まれた干し草の山を描いた一連の絵画の総称"
+	actual = arts[0].Detail
 	if actual != expected {
 		t.Errorf("got: %s\nwont: %s", actual, expected)
 	}
@@ -62,4 +105,24 @@ func randomAddress(t *testing.T) net.Addr {
 		t.Fatal(err)
 	}
 	return listener.Addr()
+}
+
+func generateToken(t *testing.T) string {
+	claims := &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Minute * 60).Unix(),
+		Subject:   "1",
+	}
+
+	keyData, _ := ioutil.ReadFile("testdata/jwtRS256.key")
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(keyData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	ss, err := token.SignedString(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return ss
 }

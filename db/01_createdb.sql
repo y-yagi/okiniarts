8eda78ecd0829f5b16968d2e6fde06e2b3f98250
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-14 14:46:01 +0900

    create DB for test

--- db/01_createdb.sql
+++ db/01_createdb.sql
@@ -1,3 +1,7 @@
 create role okiniarts login password 'okiniarts';
+
 create database okiniarts;
 grant all privileges on database okiniarts to okiniarts;
+
+create database okiniarts_test;
+grant all privileges on database okiniarts_test to okiniarts;

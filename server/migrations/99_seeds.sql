942fef4cb3ac42823532753dc95d3854711a803c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-16 15:11:56 +0900

    add seeds

---
+++ server/migrations/99_seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO artists (name, detail, user_identifier) VALUES ('クロード・モネ', '1840年11月14日 - 1926年12月5日。印象派を代表するフランスの画家。', 'google-oauth2|106274674162933758037');
+INSERT INTO arts (name, detail, user_identifier, artist_id) VALUES ('海辺の船', '', 'google-oauth2|106274674162933758037', 1);
+INSERT INTO arts (name, detail, user_identifier, artist_id) VALUES ('ルーアン大聖堂', 'ルーアンの大聖堂をモティーフに制作した連作。', 'google-oauth2|106274674162933758037', 1);

3b42c7e1b2b241433192818383e13e77944adb3d
y-yagi <y-yagi@users.noreply.github.com>
2019-08-03 13:52:03 +0900

    Test for CRUD artists

---
+++ client/cypress/fixtures/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO artists (name, detail, user_identifier) VALUES ('クロード・モネ', '1840年11月14日 - 1926年12月5日。印象派を代表するフランスの画家。', 'auth0|5d4509e60360190ec45a1f9e');
+INSERT INTO arts (name, detail, user_identifier, artist_id) VALUES ('海辺の船', '', 'auth0|5d4509e60360190ec45a1f9e', 1);
+INSERT INTO arts (name, detail, user_identifier, artist_id) VALUES ('ルーアン大聖堂', 'ルーアンの大聖堂をモティーフに制作した連作。', 'auth0|5d4509e60360190ec45a1f9e', 1);
